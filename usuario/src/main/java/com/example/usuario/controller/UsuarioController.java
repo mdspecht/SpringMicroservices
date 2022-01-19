3c7e3b3acb0f8e56d71fbbafaa127e658baa806d
Marcelo Specht <marcelo.dutra@connectinc.com.br>
2022-01-19 14:36:53 -0300

    Renomeia pacotes do serviço usuario

--- usuario/src/main/java/com/example/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/com/example/usuario/controller/UsuarioController.java
@@ -1,9 +1,8 @@
-package com.example.demo.controller;
+package com.example.usuario.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- usuario/src/test/java/com/example/usuario/UsuarioApplicationTests.java
+++ usuario/src/test/java/com/example/usuario/UsuarioApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.usuario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UsuarioApplicationTests {
 
 	@Test
 	void contextLoads() {
